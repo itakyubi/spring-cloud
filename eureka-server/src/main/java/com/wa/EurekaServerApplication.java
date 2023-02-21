21487135704878b6b704189a84bff84377a59480
wuao <18225560719@163.com>
2023-02-21 22:46:07 +0800

    eureka-server demo

---
+++ eureka-server/src/main/java/com/wa/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaServerApplication
+ * 2023/2/21 10:39 下午
+ *
+ * @author wuao
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
